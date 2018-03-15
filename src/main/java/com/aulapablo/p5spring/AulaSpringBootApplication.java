4f9a36293782f47817c97514987f7b74ab6af2d8
VDuard <viniduard_sc@hotmail.com>
2018-03-15 10:54:46 -0300

    Primeiro Commit

---
+++ src/main/java/com/aulapablo/p5spring/AulaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.aulapablo.p5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringBootApplication.class, args);
+	}
+}
